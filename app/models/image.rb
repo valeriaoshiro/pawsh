69b6d45cb4961afbe02983c2193ccc3ae70b9fba
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-07-25 13:52:59 -0700

    Map is showing with markers

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :imagetable, polymorphic: true
-  has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true, default_url: "/assets/DogProfile.png"
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
+
+# default_url: "/assets/DogProfile.png"
