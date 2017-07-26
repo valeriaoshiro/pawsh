23971708b5934a08bd41cb9b9f76b1333ab95c10
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-07-26 08:54:47 -0700

    Style pages

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,3 @@ class Image < ApplicationRecord
   has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
-
-# default_url: "/assets/DogProfile.png"
